92b51fd01eafdec4d2341b1772182d64b74fe714
Oli <oj302@bath.ac.uk>
2023-12-12 12:14:15 +0000

    Tried reading in excel file

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
-function read_file(file, out)
+function read_file(file, callback)
 {
     const reader = new FileReader();
 
@@ -17,6 +17,8 @@ function read_file(file, out)
     }
 
     reader.readAsArrayBuffer(file);
+
+    callback(out);
 }
 
 
@@ -25,8 +27,29 @@ window.addEventListener('load', function()
 {
     this.alert("page loaded");
 
+    const filePath = 'assets/ItemsForParty.xlsx';
 
-    read_file("assets/ItemsForParty.xlsx", data);
+    /*
+    function handleData(data)
+    {
+        console.log(data);
+    }
 
-    console.log(data);
+    read_file("assets/ItemsForParty.xlsx", handleData);
+    */
+
+    fetch(filePath)
+        .then(response => response.blob())
+        .then(blob => {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const data = new Uint8Array(e.target.result);
+                // Process the 'data' read from the file
+                console.log(data); // Or perform any other operations
+            };
+            reader.readAsArrayBuffer(blob);
+        })
+        .catch(error => {
+            console.error('Error fetching the file:', error);
+        });
 });
